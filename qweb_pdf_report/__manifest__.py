a71ebaa6cfd4fa9017ba1084862c6f5989c0de78
Ibrahim-Algwili <ibrahimalgwely@gmail.com>
2025-12-31 18:13:28 +0200

    report

---
+++ qweb_pdf_report/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "QWEB PDF Report ",
+    'summary': "",
+    'license': 'LGPL-3',
+    'description': """""",
+    'author': "Ibrahim Ali",
+    'version': '0.1',
+
+    'depends': ['base','student'],
+
+    'data': [
+        'report/student_report_template.xml'
+    ],
+
+}
