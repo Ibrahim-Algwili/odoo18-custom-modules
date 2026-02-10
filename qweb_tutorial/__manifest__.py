d2270b9c0777d50b7dc53ed1d0846cf8474342fa
Ibrahim-Algwili <ibrahimalgwely@gmail.com>
2026-02-10 18:22:36 +0200

    new reports

---
+++ qweb_tutorial/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "qweb_tutorial",
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'description': """
+Long description of module's purpose
+    """,
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/templates.xml',
+    ],
+
+}
+
---
+++ qweb_tutorial/controllers/controllers.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+
+class QwebTutorial(http.Controller):
+    @http.route('/qweb_tutorial', type='http', auth='public', website=True)
+    def qweb_tutorial(self):
+        return http.request.render('qweb_tutorial.python_template')
+
+
