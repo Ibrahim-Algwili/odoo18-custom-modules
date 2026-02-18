d4d4fb92b7f83ead79c8f917720460e16518015a
Ibrahim-Algwili <ibrahimalgwely@gmail.com>
2026-02-18 14:39:11 +0200

    new

--- qweb_tutorial/__manifest__.py
+++ qweb_tutorial/__manifest__.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "qweb_tutorial",
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-    'description': """
+    "name": "qweb_tutorial",
+    "summary": "Short (1 phrase/line) summary of the module's purpose",
+    "description": """
 Long description of module's purpose
     """,
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-    'category': 'Uncategorized',
-    'version': '0.1',
-    'depends': ['base'],
-
+    "author": "My Company",
+    "website": "https://www.yourcompany.com",
+    "category": "Uncategorized",
+    "version": "0.1",
+    "depends": ["base","website"],
     # always loaded
-    'data': [
+    "data": [
         # 'security/ir.model.access.csv',
-        'views/templates.xml',
+        "views/templates.xml",
     ],
-
 }
-
--- qweb_tutorial/controllers/controllers.py
+++ qweb_tutorial/controllers/controllers.py
@@ -3,8 +3,6 @@ from odoo import http
 
 
 class QwebTutorial(http.Controller):
-    @http.route('/qweb_tutorial', type='http', auth='public', website=True)
+    @http.route("/qweb_tutorial", type="http", auth="public", website=True)
     def qweb_tutorial(self):
-        return http.request.render('qweb_tutorial.python_template')
-
-
+        return http.request.render("qweb_tutorial.python_template")
